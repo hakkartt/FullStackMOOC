865fb5c323b3925b9c52572e223619b32d5825f8
Arttu Häkkinen <arttu.hakkinen@danfoss.com>
2022-06-16 13:03:59 +0300

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,14 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((total, i) => (total + i.likes), 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
