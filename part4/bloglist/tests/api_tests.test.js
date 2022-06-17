9cf7b0d9ff8afc8a7883a113a4103cedcc41ce8d
Arttu Häkkinen <arttu.hakkinen@danfoss.com>
2022-06-17 10:18:20 +0300

    4.8

---
+++ part4/bloglist/tests/api_tests.test.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  expect(response.body).toHaveLength(0)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
