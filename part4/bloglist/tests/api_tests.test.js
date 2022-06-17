667ce675f42c4b61c81764169349779a28dcdbd4
Arttu Häkkinen <arttu.hakkinen@danfoss.com>
2022-06-17 10:48:38 +0300

    4.9*

--- part4/bloglist/tests/api_tests.test.js
+++ part4/bloglist/tests/api_tests.test.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
 
 test('blogs are returned as json', async () => {
   const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-  expect(response.body).toHaveLength(0)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('blog has a field "id"', async () => {
+  const response = await api
+    .get('/api/blogs')
+  response.body.map(blog => expect(blog.id).toBeDefined())
 })
 
 afterAll(() => {
