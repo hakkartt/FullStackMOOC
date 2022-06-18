e0bfb3188074b88b56ba344aca627f04efa593c7
Arttu Häkkinen <arttu.hakkinen@danfoss.com>
2022-06-18 21:04:28 +0300

    4.20*

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -14,18 +14,25 @@ const unknownEndpoint = (request, response) => {
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
-
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
-
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
