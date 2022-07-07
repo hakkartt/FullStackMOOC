eb2507227e536a093326fbf0a23ca97600943abe
Arttu Häkkinen <arttu.hakkinen@danfoss.com>
2022-07-07 09:48:41 +0300

    5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.spec.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.spec.cy.js
@@ -2,6 +2,12 @@ describe('Blog app', function() {
 
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      user: 'testuser',
+      password: 'testpassword',
+      username: 'testusername'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,6 +16,28 @@ describe('Blog app', function() {
     cy.get('#username')
     cy.get('#password')
     cy.get('#loginButton')
+  })
+
+  describe('Login',function() {
+
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testusername')
+      cy.get('#password').type('testpassword')
+      cy.get('#loginButton').click()
+      cy.get('html')
+        .should('contain', 'Logged in testusername')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testusername')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#loginButton').click()
+      cy.get('html')
+        .should('not.contain', 'Logged in testusername')
+      cy.get('.error')
+        .should('contain', 'Invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 
 })
